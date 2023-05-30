89c14afa6d7045743b279c7157f6fcabf0c8d5cb
JiHyuk Choi <116854709+zhyuk@users.noreply.github.com>
2023-05-30 10:18:07 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
-//bg13 script
+// user_section script
+// let box1 = document.getElementById("user_box1");
+// let box2 = document.getElementById("user_box2");
+// let box3 = document.getElementById("user_box3");
 
-const scrollPos = window.scrollY;
-console.log(scrollPos);
+// user_section script end
 
-let element = document.getElementById("bg13_text");
+// function3_section script
+// let test1 = documnet.getElementById("drug1")
+// let pos = -23 + "%";
+
+// let drug = setInterval(function(){
+//     pos -= 1;
+//     test1.style.left = pos + "%";
+
+// },100);
+// function3_section script end
+
+// service_section script
+let element = document.getElementById("service_section_text");
 let opacity = 0;
 let scalePos = 0.5;
 
@@ -20,4 +34,5 @@ let timer = setInterval(function () {
     }
 
 
-}, 100);
+}, 85);
+// service_section script end
